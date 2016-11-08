ba23b3e0c81830a2c97067ae594c9a12ca870acc
developernick <nickolas.selesky@gmail.com>
2016-11-08 13:58:21 -0600

    Starting Exercise

---
+++ script.js
@@ -0,0 +1,21 @@
+create a function that will loop through the string
+
+if the letter is a-m ignore
+if the letter is n-z error
+
+result will be errors/length of string
+
+
+
+
+
+
+
+
+
+
+
+
+function printerError(s) {
+    // your code
+}
