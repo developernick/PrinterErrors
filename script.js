5592a3f4ca42bfcb43c60b4b8d241fb3689ee00b
developernick <nickolas.selesky@gmail.com>
2016-11-08 14:28:24 -0600

    Finished Exercise

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-create a function that will loop through the string
-
-if the letter is a-m ignore
-if the letter is n-z error
-
-result will be errors/length of string
-
-
-
-
-
-
-
-
-
-
-
+// create a function (printerError) that will loop through the string (s)
+//
+// if the letter is a-m ignore
+// if the letter is n-z error
+//
+// result will be errors/length of string
 
 function printerError(s) {
-    // your code
+	var passingLetters = ("abcdefghijklm").split("");// a base array of acceptable letters
+	var a = s.length;//this will be the denominator in the final result
+	var i = 0;//index number for the while loop to pass throught the string
+	var count = 0;//this will be the numerator in the final result
+
+	while (i<a)
+	{
+		if (passingLetters.indexOf(s[i])===-1) {
+			count++;
+		}
+		i++;
+	}
+	return (count + "/" + a);
 }
